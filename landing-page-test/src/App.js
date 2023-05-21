e2b66ee6e70d650e7bdbf2df90311bfbb1a8b416
github username <github email address>
2023-05-21 18:11:42 -0400

    Positioning of objects in parallax scroll

--- landing-page-test/src/App.js
+++ landing-page-test/src/App.js
@@ -10,27 +10,40 @@ function App() {
   return (
     <div className='App'>
       <section>
-        <nav>
-          <div>
-            <a href='https://www.nytimes.com/'>
-              The New York Times
-            </a>
-          </div>
-        </nav>
       </section>
         <div className='container'>
           <div className='wrapper'>
             <section className='slide'>
-              <div className='layer l1'>
+              <div className='layer1'>
+              <nav>
+              <div className='nytLink'>
+                <a href='https://www.nytimes.com/'>
+                  The New York Times
+                </a>
+              </div>
+            </nav>
                 <p>{`Following the threads of our interests and passions, wherever they take us, is exhilarating. That’s why Times journalism not only reports on the issues that matter but illuminates how they’re connected — so we can marvel at the complexity of our world as we come to understand it better.`}</p>
                 <br></br>
                 <p>{`More of life brought to life.`}</p>
               </div>
             </section>
             <section className='slide'>
-              <h1>PLACE YOUR CONTENT HERE</h1>
-              <img src={airJordan} alt='test'></img>
-              <div className='layer l3'></div>
+            <h1>{'If you understand Sneakers'}</h1>
+              <div className='layer2'>
+                <img src={airJordan} alt='test'></img>
+                <div className='link'>
+                  <a href='https://www.nytimes.com/interactive/2021/10/15/style/sneaker-bots.html'>
+                    <h4>Styles</h4>
+                    <h3>The Fight for Sneakers</h3>
+                    <p>By Daisuke Wakabayashi</p>
+                  </a>
+                </div>
+              </div>
+            </section>
+            <section className='slide'>
+              <div className='lastSlide'>
+                <h2>{'More of life brought to life.'}</h2>
+              </div>
             </section>
           </div>
         </div>
